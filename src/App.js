96c6bec6dbd5a4f3df7a330357bddcf831e56103
DominicSherman <dominic.sherman98@gmail.com>
2018-11-28 20:18:49 -0600

    webscraping stuff

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {getData} from './webscraping-service';
 
 class App extends Component {
-    componentDidMount() {
-        getData();
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            matches: []
+        }
+    }
+
+    async componentDidMount() {
+        const matches = await getData();
+        console.log('matches', matches);
+        this.setState({matches});
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                {
+                    this.state.matches.map((m) => (
+                        <div className={'App-rowWrapper'}>
+                            <div className={'App-columnWrapper'}>
+                                <img
+                                    alt={''}
+                                    src={m.homeLogo}
+                                />
+                            </div>
+                            <div className={'App-columnWrapper'}>{m.home}</div>
+                            <div className={'App-columnWrapper'}>
+                                <div>{m.time}</div>
+                                <div>{m.homeScore}-{m.awayScore}</div>
+                            </div>
+                            <div className={'App-columnWrapper'}>{m.away}</div>
+                            <div className={'App-columnWrapper'}>
+                                <img
+                                    alt={''}
+                                    src={m.awayLogo}
+                                />
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         );
     }
--- src/webscraping-service.js
+++ src/webscraping-service.js
@@ -3,17 +3,49 @@ import $ from 'cheerio';
 
 const url = 'http://www.espn.com/soccer/team/_/id/9723';
 
-export const getData = () => {
-    rp(url)
+const getTime = (times, i) => {
+    if (times[`${i/2}`].children[0].data) {
+        return times[`${i/2}`].children[0].data
+    }
+
+    const date = new Date(times[`${i/2}`].children[0].attribs['data-date']);
+
+    return (`${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`)
+};
+
+
+export const getData = async () => {
+    return rp(url)
         .then((html) => {
             const names = $('span.short-name', html);
+            const homeScores = $('span.score.icon-font-after', html);
+            const awayScores = $('span.score.icon-font-before', html);
+            const times = $('span.game-time', html);
+            const scrapedLogos = $('img.team-logo', html);
 
-            console.log('names', names);
-            let matches = [];
-            names.forEach((n, index) => {
-                if (index % 2 === 1) {
-                    console.log('n.children.data', n.children.data);
+            let logos = [];
+
+            for (let j = 1; j < scrapedLogos.length - 1; j = j + 2) {
+                console.log('j', j);
+                if (scrapedLogos[j].attribs.src !== scrapedLogos[j + 1].attribs.src) {
+                    logos = [...logos, scrapedLogos[j].attribs.src];
                 }
-            });
+            }
+
+            let matches = [];
+
+            for (let i = 0; i < names.length; i = i + 2) {
+                matches = [...matches, {
+                    home: names[`${i}`].children[0].data,
+                    away: names[`${i + 1}`].children[0].data,
+                    homeScore: homeScores[`${i/2}`].children[0].data,
+                    awayScore: awayScores[`${i/2}`].children[0].data,
+                    homeLogo: logos[i],
+                    awayLogo: logos[i + 1],
+                    time: getTime(times, i)
+                }];
+            }
+
+            return matches;
         })
 };
